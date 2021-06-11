36c3533c9a5832149bfc3896ac96cbeaa65dcc5f
Susie-cho <80011876+sujicho@users.noreply.github.com>
2021-06-11 14:22:16 +1200

    FInish html and css

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ server.use(express.urlencoded({extended: false}))
 server.engine('hbs', hbs({ extname: 'hbs'}))
 server.set('view engine', 'hbs')
 
-// const staticPath = path.join(__dirname, 'public')
-// server.use(express.static(staticPath))
+// these two lines need to be here to use static folder (public, images etc)
+const staticPath = path.join(__dirname, 'public')
+server.use(express.static(staticPath))
 
 server.use('/', routes)
